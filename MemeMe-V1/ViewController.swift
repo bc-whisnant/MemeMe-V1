1c0a89b554dc4f649e84a6ebf47931ce9ad0d048
Brandon Whisnant <bc.whisnant@gmail.com>
2018-01-01 13:55:44 -0500

    Added text field attributes - tested in all orientations and it works without issue.

--- MemeMe-V1/ViewController.swift
+++ MemeMe-V1/ViewController.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    //declare variables here
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var pickedImageView: UIImageView!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var libraryButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    
+    //added text attributes for text fields
+    //added code for the text field styles
+    let memeTextAttributes:[String:Any] = [
+        NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+        NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+        NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedStringKey.strokeWidth.rawValue: 4]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //set default attributes for text fields and assign delegates to each
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        //this sets the code to center align ---> this has to be done via code
+        topTextField.textAlignment = .center
+        bottomTextField.textAlignment = .center
+        topTextField.delegate = self
+        bottomTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //this function allows the return button to dismiss the keyboard
+    func textFieldShouldReturn(_ topTextField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
 
 
